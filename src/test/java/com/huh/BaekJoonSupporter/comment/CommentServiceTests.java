d9d8ee64502bfe86e6ba6155c4c1677378147b6e
PARKPARKWOO <wy9295@naver.com>
2023-04-11 14:28:07 +0900

    test good

--- src/test/java/com/huh/BaekJoonSupporter/comment/CommentServiceTests.java
+++ src/test/java/com/huh/BaekJoonSupporter/comment/CommentServiceTests.java
@@ -37,25 +37,6 @@ public class CommentServiceTests {
     @Test
     @Rollback(value = false)
     void CommentTests(){
-        Member member = new Member();
-        Long boId = this.boardService.create("제목", "내용", member);
-        Board board = new Board()
-        // 댓글 생성
-        Comment comment = this.commentService.create("테스트 댓글", boardService.getBoard(10L), memberService.getMember("park"));
-        assertThat(comment.getContent()).isEqualTo("테스트 댓글");
 
-//         댓글 수정
-        Optional<Comment> checkco = this.commentRepository.findByContent("테스트 댓글");
-        if (!checkco.isPresent());
-        Comment comment1 = checkco.get();
-        commentService.modify(comment1, "수정");
-        assertThat(comment1.getContent()).isEqualTo("수정");
-
-//        댓글 삭제
-        Optional<Comment> checkco1 = this.commentRepository.findByContent("수정");
-        if (!checkco1.isPresent());
-        Comment comment12 = checkco1.get();
-        commentService.delete(comment12);
-        assertTrue(!comment12.getContent().isEmpty());
     }
 }
